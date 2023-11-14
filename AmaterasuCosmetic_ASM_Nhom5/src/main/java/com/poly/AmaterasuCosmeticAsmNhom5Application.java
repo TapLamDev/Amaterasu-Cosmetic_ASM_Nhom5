0e154284b23f1fe03a7e12e56417a52f4a217376
tuimoihoc <hoangmystara123@gmail.com>
2023-11-14 16:12:19 +0700

    //tạo github

---
+++ AmaterasuCosmetic_ASM_Nhom5/src/main/java/com/poly/AmaterasuCosmeticAsmNhom5Application.java
@@ -0,0 +1,13 @@
+package com.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmaterasuCosmeticAsmNhom5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmaterasuCosmeticAsmNhom5Application.class, args);
+	}
+
+}
